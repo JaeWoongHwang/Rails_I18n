61657b59b815b63de1211b67b43294583e91edcf
JaeWoongHwang <jwngh15@gmail.com>
2018-06-06 23:38:12 +0900

    First Commit

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module FanmadeLandingPage
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.i18n.default_locale = :ko
+    config.i18n.available_locales = [:ko, :en, :'zh-CN', :ja]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}' ).to_s]
+    config.i18n.enforce_available_locales = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  match '/:locale' => 'posts#index', via: [:get]
+
+  scope "{:locale}", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :posts
+    root 'posts#index'
+  end
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
